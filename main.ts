467ce1d15080c02ecece3a7020df4350b9986051
Yumberries <67992156+Yumberries@users.noreply.github.com>
2024-10-15 18:11:01 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,6 +11,25 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         grid.move(mySprite, 0, -1)
     }
 })
+function Delete_used_card_from_hand (Name: string) {
+    if (Hand[0] == Name && Card_Cleared == false) {
+        Hand[0] = ""
+        Card_Cleared = true
+    } else if (Hand[1] == Name && Card_Cleared == false) {
+        Hand[1] = ""
+        Card_Cleared = true
+    } else if (Hand[2] == Name && Card_Cleared == false) {
+        Hand[2] = ""
+        Card_Cleared = true
+    } else if (Hand[3] == Name && Card_Cleared == false) {
+        Hand[3] = ""
+        Card_Cleared = true
+    } else if (Hand[4] == Name && Card_Cleared == false) {
+        Hand[4] = ""
+        Card_Cleared = true
+    }
+    Card_Cleared = false
+}
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (B_Pressed == 0) {
         CanMove = false
@@ -20,7 +39,7 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
         Hand[2],
         Hand[3],
         Hand[4]
-        ], MenuStyle.List, MenuLocation.TopHalf)
+        ], MenuStyle.List, MenuLocation.BottomHalf)
         B_Pressed = 1
     } else if (B_Pressed == 1) {
         blockMenu.closeMenu()
@@ -28,9 +47,14 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
         CanMove = true
     }
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
-	
-})
+function Give_Tower_Healthbar (Sprite2: Sprite, Strengh: number, Health: number) {
+    statusbar = statusbars.create(10, 2, StatusBarKind.Health)
+    statusbar.max = Health
+    statusbar.value = Health
+    statusbar.attachToSprite(Sprite2, 0, 0)
+    statusbar.setLabel("" + Strengh + "/" + Health, 15)
+    statusbar.setColor(2, 12, 13)
+}
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     if (CanMove == true) {
         grid.move(mySprite, -1, 0)
@@ -55,7 +79,29 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Tower, function (sprite, otherSp
         1 . . 1 . . . . . . . . 1 . . 1 
         1 1 1 1 . . 1 1 1 1 . . 1 1 1 1 
         `) && controller.A.isPressed()) {
-    	
+        statusbars.getStatusBarAttachedTo(StatusBarKind.Health, otherSprite).value += -1
+        mySprite.setImage(img`
+            1 1 1 1 . . 1 1 1 1 . . 1 1 1 1 
+            1 . . 1 . . . 1 1 . . . 1 . . 1 
+            1 . . . . . . . . . . . . . . 1 
+            1 1 . . . . . . . . . . . . 1 1 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            1 . . . . . . . . . . . . . . 1 
+            1 1 . . . . . . . . . . . . 1 1 
+            1 1 . . . . . . . . . . . . 1 1 
+            1 . . . . . . . . . . . . . . 1 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            1 1 . . . . . . . . . . . . 1 1 
+            1 . . . . . . . . . . . . . . 1 
+            1 . . 1 . . . 1 1 . . . 1 . . 1 
+            1 1 1 1 . . 1 1 1 1 . . 1 1 1 1 
+            `)
+        if (statusbars.getStatusBarAttachedTo(StatusBarKind.Health, otherSprite).value <= 0) {
+            sprites.destroy(otherSprite)
+            sprites.destroy(statusbars.getStatusBarAttachedTo(StatusBarKind.Health, otherSprite))
+        }
     }
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -68,6 +114,25 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         grid.move(mySprite, 0, 1)
     }
 })
+function Draw_Card (Amount: number) {
+    if (Hand[0] == "" && Cards_Drawn < Amount) {
+        Hand[0] = Cards._pickRandom()
+        Cards_Drawn += 1
+    } else if (Hand[1] == "" && Cards_Drawn < Amount) {
+        Hand[1] = Cards._pickRandom()
+        Cards_Drawn += 1
+    } else if (Hand[2] == "" && Cards_Drawn < Amount) {
+        Hand[2] = Cards._pickRandom()
+        Cards_Drawn += 1
+    } else if (Hand[3] == "" && Cards_Drawn < Amount) {
+        Hand[3] = Cards._pickRandom()
+        Cards_Drawn += 1
+    } else if (Hand[4] == "" && Cards_Drawn < Amount) {
+        Hand[4] = Cards._pickRandom()
+        Cards_Drawn += 1
+    }
+    Cards_Drawn = 0
+}
 blockMenu.onMenuOptionSelected(function (option, index) {
     if (blockMenu.selectedMenuOption() == "G1| 1/2 Red" && info.score() >= 1 && B_Pressed == 1) {
         info.changeScoreBy(-1)
@@ -91,6 +156,7 @@ blockMenu.onMenuOptionSelected(function (option, index) {
             1 . . 1 . . . . . . . . 1 . . 1 
             1 1 1 1 . . 1 1 1 1 . . 1 1 1 1 
             `)
+        Delete_used_card_from_hand("G1| 1/2 Red")
         blockMenu.closeMenu()
     } else if (blockMenu.selectedMenuOption() == "G2| 2/1 Blue" && info.score() >= 2 && B_Pressed == 1) {
         info.changeScoreBy(-2)
@@ -114,6 +180,7 @@ blockMenu.onMenuOptionSelected(function (option, index) {
             1 . . 1 . . . . . . . . 1 . . 1 
             1 1 1 1 . . 1 1 1 1 . . 1 1 1 1 
             `)
+        Delete_used_card_from_hand("G2| 2/1 Blue")
         blockMenu.closeMenu()
     } else if (blockMenu.selectedMenuOption() == "G3| 2/3 Yellow" && info.score() >= 3 && B_Pressed == 1) {
         info.changeScoreBy(-3)
@@ -137,8 +204,9 @@ blockMenu.onMenuOptionSelected(function (option, index) {
             1 . . 1 . . . . . . . . 1 . . 1 
             1 1 1 1 . . 1 1 1 1 . . 1 1 1 1 
             `)
+        Delete_used_card_from_hand("G3| 2/3 Yellow")
         blockMenu.closeMenu()
-    } else if (blockMenu.selectedMenuOption() == "G2| Creature takes 2 damage" && info.score() >= 2 && B_Pressed == 1) {
+    } else if (blockMenu.selectedMenuOption() == "G2| Creature gets -0/-1" && info.score() >= 2 && B_Pressed == 1) {
         info.changeScoreBy(-2)
         CanMove = true
         B_Pressed = 0
@@ -160,23 +228,30 @@ blockMenu.onMenuOptionSelected(function (option, index) {
             1 . . 1 . . . . . . . . 1 . . 1 
             1 1 1 1 . . 1 1 1 1 . . 1 1 1 1 
             `)
+        Delete_used_card_from_hand("G2| Creature gets -0/-1")
         blockMenu.closeMenu()
-    } else if (blockMenu.selectedMenuOption() == "G1| Gain 1 Life" && info.score() >= 1 && B_Pressed == 1) {
-        info.changeScoreBy(-1)
+    } else if (blockMenu.selectedMenuOption() == "G2| Gain 1 Life" && info.score() >= 2 && B_Pressed == 1) {
+        info.changeScoreBy(-2)
         CanMove = true
         B_Pressed = 0
         info.changeLifeBy(1)
+        Delete_used_card_from_hand("G2| Gain 1 Life")
         blockMenu.closeMenu()
     }
 })
 function Start_Turn () {
     info.setScore(randint(3, 10))
+    Draw_Card(1)
 }
 let YellowTower: Sprite = null
 let BlueTower: Sprite = null
 let RedTower: Sprite = null
+let Cards_Drawn = 0
+let statusbar: StatusBarSprite = null
+let Card_Cleared = false
 let B_Pressed = 0
 let Hand: string[] = []
+let Cards: string[] = []
 let CanMove = false
 let mySprite: Sprite = null
 tiles.setCurrentTilemap(tilemap`level2`)
@@ -202,19 +277,19 @@ grid.snap(mySprite)
 blockMenu.setColors(12, 1)
 CanMove = true
 scene.centerCameraAt(150, 72)
-let Cards = [
+Cards = [
 "G1| 1/2 Red",
 "G2| 2/1 Blue",
 "G3| 2/3 Yellow",
-"G2| Creature takes 2 damage",
-"G1| Gain 1 Life"
+"G2| Creature gets -0/-1",
+"G2| Gain 1 Life"
 ]
 Hand = [
-Cards[0],
-Cards[1],
-Cards[2],
-Cards[3],
-Cards[4]
+Cards._pickRandom(),
+Cards._pickRandom(),
+Cards._pickRandom(),
+Cards._pickRandom(),
+Cards._pickRandom()
 ]
 B_Pressed = 0
 blockMenu.setControlsEnabled(true)
@@ -270,6 +345,7 @@ game.onUpdate(function () {
             `, SpriteKind.Tower)
         tiles.placeOnTile(RedTower, mySprite.tilemapLocation())
         tiles.setTileAt(mySprite.tilemapLocation(), assets.tile`myTile0`)
+        Give_Tower_Healthbar(RedTower, 1, 2)
     } else if (mySprite.image.equals(img`
         1 1 1 1 . . 1 1 1 1 . . 1 1 1 1 
         1 . . 1 . . . . . . . . 1 . . 1 
@@ -320,6 +396,7 @@ game.onUpdate(function () {
             `, SpriteKind.Tower)
         tiles.placeOnTile(BlueTower, mySprite.tilemapLocation())
         tiles.setTileAt(mySprite.tilemapLocation(), assets.tile`myTile0`)
+        Give_Tower_Healthbar(BlueTower, 2, 1)
     } else if (mySprite.image.equals(img`
         1 1 1 1 . . 1 1 1 1 . . 1 1 1 1 
         1 . . 1 . . . . . . . . 1 . . 1 
@@ -370,5 +447,6 @@ game.onUpdate(function () {
             `, SpriteKind.Tower)
         tiles.placeOnTile(YellowTower, mySprite.tilemapLocation())
         tiles.setTileAt(mySprite.tilemapLocation(), assets.tile`myTile0`)
+        Give_Tower_Healthbar(YellowTower, 2, 3)
     }
 })
